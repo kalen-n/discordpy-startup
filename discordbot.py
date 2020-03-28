30bb1ae66440c98d4d4ede62ad4022f6846666df
Kalen-ne <54280054+Kalen-ne@users.noreply.github.com>
2020-03-28 19:17:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,15 +2,17 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client.event
+async def on_message(message):
+    if message.content.statswith('kingariempire'):
+        role = discord.utils.get(message.guild.roles, name= '国民')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} ようこそ！'
+        await message.channel.send(reply)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
 
 @bot.command()
